d8ec9d41b2a4cd01b9579a92f34a8e948ff2a1b0
J. Austin Hughey <jah@jah.io>
2020-04-07 02:05:59 -0600

    disable host check

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   devServer: {
     headers: {
       'Cache-Control': 'no-store'
-    }
+    },
+    disableHostCheck: true
   },
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'development') {
